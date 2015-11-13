e102d3ae049662e77ba9ba9e9f9cf0627313b38c
Kelly Dwan <kelly.dwan@gmail.com>
2015-11-13 11:51:27 -0500

    Get options from a config object in addition to query params on the loader string.

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-var loaderUtils = require('loader-utils');
+var utils = require('loader-utils');
+var assign = require('object-assign');
 
 module.exports = function( content ) {
-	var query = loaderUtils.parseQuery( this.query );
+	var options = getLoaderConfig( this );
 	this.cacheable && this.cacheable();
 
-	if ( 'undefined' === typeof query.file ) {
+	if ( 'undefined' === typeof options.file ) {
 		return content;
 	}
 
 	return content.replace(
 		/@import \"(\.\.\/)*scss\/calypso-colors\";/ig,
-		'$&\n@import "' + query.file + '";'
+		'$&\n@import "' + options.file + '";'
 	);
 };
+
+/**
+ * Check the loader query and webpack config for loader options. If an option is defined in both places,
+ * the loader query takes precedence.
+ *
+ * [from sass-loader: https://github.com/jtangelder/sass-loader/blob/master/index.js#L377]
+ *
+ * @param {Loader} loaderContext
+ * @returns {Object}
+ */
+function getLoaderConfig( loaderContext ) {
+	var query = utils.parseQuery( loaderContext.query );
+	var configKey = query.config || 'customColorsLoader';
+	var config = loaderContext.options[configKey] || {};
+
+	delete query.config;
+
+	return assign( query, config );
+}
